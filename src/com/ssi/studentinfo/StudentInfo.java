554beeba9b5504ea087e31f5347c4eafe31398fd
Rishabh Handay <rishabhhanday.rh@gmail.com>
2019-08-26 23:39:34 +0530

    26/08

--- src/com/ssi/studentinfo/StudentInfo.java
+++ src/com/ssi/studentinfo/StudentInfo.java
@@ -1,7 +1,7 @@
 package com.ssi.studentinfo;
 
 import info.student.Student;
-
+//
 public class StudentInfo {
 public static void main(String...sd){
 	new Student().studentInfo("Rishabh Handay",23);
